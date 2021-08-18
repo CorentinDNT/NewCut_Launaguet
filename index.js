459cc1d141957555de157faccc7561385bf02be3
CorentinDnt <donatocorentin2@gmail.com>
2021-08-19 01:11:40 +0200

    Add toggle for Prices
    next commit -> animate this sh*t

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const buttonBurgerMenu = document.getElementById("icon");
-const addClassShow = document.getElementById("whereToAddShowClass");
+const addClassShowBurger = document.getElementById("whereToAddShowClass");
 
-function toggleClass() {
-	if (addClassShow.classList == "show") {
-		addClassShow.classList.remove("show");
-		console.log(addClassShow.classList);
+function toggleClassBurger() {
+	if (addClassShowBurger.classList == "show") {
+		addClassShowBurger.classList.remove("show");
+		console.log(addClassShowBurger.classList);
 	} else {
-		addClassShow.classList.add("show");
-		console.log(addClassShow.classList);
+		addClassShowBurger.classList.add("show");
+		console.log(addClassShowBurger.classList);
+	}
+}
+
+const addClassPriceList = document.getElementsByClassName(
+	"whereToAddShowPrice"
+);
+
+const miss = document.getElementById("miss");
+const sir = document.getElementById("sir");
+
+function toggleClassMiss() {
+	if (miss.classList == "show") {
+		miss.classList.remove("show");
+		console.log(miss.classList);
+	} else {
+		miss.classList.add("show");
+		console.log(miss.classList);
+	}
+}
+function toggleClassMister() {
+	if (sir.classList == "show") {
+		sir.classList.remove("show");
+		console.log(sir.classList);
+	} else {
+		sir.classList.add("show");
+		console.log(sir.classList);
 	}
 }
